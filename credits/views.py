db5bcd2d913e67b0c4e4ba010ac6fcfb7e7e3a7f
jheisonxxx <jheison.talavera@gmail.com>
2021-10-10 23:09:05 -0500

    readme

--- credits/views.py
+++ credits/views.py
@@ -8,15 +8,15 @@ from rest_framework import viewsets
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    search_fields = ['dni', 'name', 'punctuation','debt_sbs']
+    ordering = ['id']
+    filterset_fields = {'dni': ['exact', 'gte', 'lte','gt', 'lt'],
                         'name': ['exact', 'gte', 'lte', 'gt', 'lt'],
                         'punctuation': ['exact', 'gte', 'lte', 'gt', 'lt'],
                         'debt_sbs': ['exact', 'gte', 'lte', 'gt', 'lt'],
     }
                         
 class SolicitudeViewSet(viewsets.ModelViewSet):
-    search_fields = ['dni', 'name', 'punctuation','debt_sbs']
-    ordering = ['id']
-    filterset_fields = {'dni': ['exact', 'gte', 'lte','gt', 'lt'],
     queryset = Solicitude.objects.all()
     serializer_class = SolicitudeSerializer
     search_fields = ['state','credit_indicator','client']
